ba7536df0beebd898911dac84bc5ae17e22f4857
Aryan0616 <dubeyaryan82@gmail.com>
2024-11-28 14:52:49 +0530

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
@@ -12,10 +13,10 @@ app.use(bodyParser.json());
 
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root", 
-    password: "@Misfits13", 
-    database: "productinate",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
